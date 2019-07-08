29c4d7a7acecae6efe6d8d831896a46499182954
LeoKingMnM <1502630424@qq.com>
2019-07-08 21:15:53 +0800

    删除功能实现

--- src/views/heroes/HeroList.vue
+++ src/views/heroes/HeroList.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
         <h2 class="sub-header">英雄列表</h2>
-        <a class="btn btn-success" href="add.html">Add</a>
+        <a class="btn btn-success" href="add.html">添加人物</a>
         <div class="table-responsive">
           <table class="table table-striped">
             <thead>
@@ -13,14 +13,14 @@
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td>1</td>
-                <td>Lorem</td>
-                <td>女</td>
+              <tr v-for="(item,index) in list" :key="item.id">
+                <td>{{ index + 1 }}</td>
+                <td>{{ item.name }}</td>
+                <td>{{ item.gender }}</td>
                 <td>
                   <a href="edit.html">添加</a>
                   &nbsp;&nbsp;
-                  <a href="javascript:;">删除</a>
+                  <a href="javascript:;" @click="del(item.id)">删除</a>
                 </td>
               </tr>
             </tbody>
@@ -29,7 +29,43 @@
       </div>
 </template>
 <script>
+import axios from 'axios'
 export default {
-    
+  data() {
+    return {
+      list:[]
+    }
+  },
+ mounted(){
+    this.loadData()
+  },
+  methods:{
+    loadData() {
+      axios
+        .get('http://localhost:3000/heroes')
+        .then(response=>{
+          const { data , status } = response
+          if (status == 200) {
+            this.list = data
+          }
+        })
+    },
+    del(id){
+      //删除提示
+      if (!confirm('确定删除吗')) {
+        return false
+      }
+      axios
+        .delete(`http://localhost:3000/heroes/${id}`)
+        .then(response => {
+          const { status } = response
+          if (status == 200) {
+            this.loadData()
+          }else{
+            alert('失败')
+          }
+        })
+    }
+  },
 }
 </script>
